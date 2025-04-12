37631e2cb7d58a100f80e74548673cd285d890c4
davidchau21 <davidduongxu1@gmail.com>
2025-04-12 14:58:43 +0700

    add header

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['s2.coinmarketcap.com', 's3.coinmarketcap.com', 'cdn.sanity.io', 'encrypted-tbn0.gstatic.com']
+  },
 };
 
 export default nextConfig;
